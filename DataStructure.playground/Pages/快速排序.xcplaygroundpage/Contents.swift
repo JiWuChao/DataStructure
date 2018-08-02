d53a4bc5212f9a0f7ee7b00d6e9ef281367027d1
jiwuchao <wuchaoji@pptv.com>
2018-08-02 14:55:14 +0800

    快速排序

--- DataStructure.playground/Pages/快速排序.xcplaygroundpage/Contents.swift
+++ DataStructure.playground/Pages/快速排序.xcplaygroundpage/Contents.swift
@@ -3,120 +3,72 @@
 import Foundation
 
 
-
-/// 分开两个数组的 index
-///
-/// - Parameters:
-///   - contents: <#contents description#>
-///   - sortBy: <#sortBy description#>
-///   - low: <#low description#>
-///   - high: <#high description#>
-/// - Returns: <#return value description#>
-func quickSeparate<T: Comparable>(_ contents:inout [T],_ sortBy:(T,T)->Bool, low: Int, high: Int) -> Int {
-    assert(contents.count > 1, "数组无需排序")
-    assert(low >= 0, "下标不能小与0")
-    assert(high < contents.count, "下标不能越界")
-    assert(low < high, "低位不能大于高位")
-    
-    let pivot = contents[high] //选中一个高位
+/*
+ 快速排序
+ */
+func quicklySort<T: Comparable>(_ contents:inout [T], low: Int, high: Int) {
     var i = low
-    for j in low..<high {//从低到高的下标区间
-        if sortBy(contents[j],pivot) {//根据下标取出值
-            contents.swapAt(j, i)
+    var j = high
+    
+    let piv = (low + (high - low) / 2)
+    let lowValue = contents[piv] ////标志位
+    
+    /*
+     一个循环的流程
+     
+     
+     从高位（右边）开始：
+     1 循环判断 当前高位的值是否大于标志位值 如果大于 则再向前一位比较 一直找到小于标志位的index
+     2 高位找到比标志位的值小的index 之后 停止开始从低位（左边）开始
+     
+     低位开始 ：
+     
+     1 循环判断 当前低位的值是否小于标志位 如果小于标志位则index + 1 进行下一位比较，直到找到比标志位大的值
+     2 低位找到比标志位大的值之后停止
+     
+     交换：
+     
+     从低位和高位找到了 比标志位大的值和比标志位小的值 然后这两个值交换
+     
+     当i >= j 即低位的index >= 高位的index时 停止这一轮的循环
+     
+     */
+    
+    print("\n标志位")
+    print(lowValue)
+    
+    while i < j {
+        while contents[j] >  lowValue {
+            j -= 1//高位的 index - 1
+        }
+        while contents[i] < lowValue {
             i += 1
         }
-        print(i)
-    }
-    contents.swapAt(i, high)
-    print(contents)
-    return i
-}
-
-func partition<T: Comparable>(_ contents:inout [T],_ sortBy:(T,T)->Bool, low: Int, high: Int) -> Int  {
-    var low = low
-    var high = high
-    assert(contents.count > 1, "数组无需排序")
-    assert(low >= 0, "下标不能小与0")
-    assert(high < contents.count, "下标不能越界")
-    assert(low < high, "低位不能大于高位")
-    let lowValue = contents[low]
-//    print(pro)
-    while low < high {
-        while low < high && contents[high] >= lowValue {
-            high -= 1
-            contents[low] = contents[high]
-            print("----")
-        }
-        while low < high && contents[low] <= lowValue {
-            low += 1
-            contents[high] = contents[low]
+        if i <= j  {
+           contents.swapAt(i, j)
+            i += 1
+            j -= 1
         }
     }
-     contents[low] = lowValue
-    print("low: \(low)")
-    print("high: \(high)")
-    return low
-}
-
-func quicklySort<T: Comparable>(_ contents:inout [T],_ sortBy:(T,T)->Bool, low: Int, high: Int) {
-    if low < high {
-        let pivotloc = partition(&contents, sortBy, low: low, high: high)
-        quicklySort(&contents, sortBy, low: low, high: pivotloc - 1)
-        quicklySort(&contents, sortBy, low: pivotloc + 1, high: high)
-        print(contents)
+    print("本轮排序结果:\n\(contents)")
+    print("\n----------下一轮分割线--------------")
+    print("low = \(low),high = \(high), i = \(i),j = \(j)")
+    if low < j {
+        print("\n----------左边--------------")
+        quicklySort(&contents, low: low, high: j)
+    }
+    if i < high {
+        print("\n----------右边--------------")
+        quicklySort(&contents, low: i, high: high)
     }
 }
 
 
-
-
-var arr2 = [1,32,2,55,6,3,44]
-
+var arr2 = [7,32,2,1,5,6,3,44]
+print("初始化数组")
 print(arr2)
+quicklySort(&arr2, low: 0, high: arr2.count - 1)
 
-quicklySort(&arr2, { (v1, v2) -> Bool in
-    return v1 >= v2
-}, low: 0, high: arr2.count - 1)
-print(arr2)
-
-
-/*
- void main ( )
- {   QSort ( L, 1, L.length ); }
-
- void QSort ( SqList &L，int low,  int  high ) {
-   if  ( low < high ) {
-    pivotloc = Partition(L, low, high ) ;
-    Qsort (L, low, pivotloc-1) ;
-    Qsort (L, pivotloc+1, high )
-  }
- }
-
- 
- int Partition ( SqList &L，int low,  int  high ) {
-  L.r[0] = L.r[low];
- pivotkey = L.r[low].key;
- while  ( low < high ) {
-  while ( low < high && L.r[high].key >= pivotkey )
-   --high;
- L.r[low] = L.r[high];
-    while ( low < high && L.r[low].key <= pivotkey )
-        ++low;
-        L.r[high] = L.r[low];
- }
- L.r[low]=L.r[0];
- return low;
- }
- 
- */
-//while low < high && contents[high] >= pro {
-//    high -= 1
-//    contents[low] = contents[high]
-//}//contents[low] <= pro
-//while low < high && contents[low] <= pro {
-//    low += 1
-//    contents[high] = contents[low]
-//}
 
 
 
